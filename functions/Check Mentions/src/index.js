c1dd8d9f383cdfe3479c33697a2d7d7a0a5b8ba7
Ranvir Choudhary <ranvir2002choudhary@gmail.com>
2023-05-17 13:25:28 +0530

    Check Mentions works now!

--- functions/Check Mentions/src/index.js
+++ functions/Check Mentions/src/index.js
@@ -1,39 +1,39 @@
-const { Client, Databases, Query } = require("node-appwrite");
+const { Databases, Client, Query} = require("node-appwrite");
 const { TwitterApi } = require("twitter-api-v2");
 
 module.exports = async function (req, res) {
   const client = new Client();
-
+  
   if (
     !req.variables['APPWRITE_FUNCTION_ENDPOINT'] ||
-    !req.variables['APPWRITE_FUNCTION_API_KEY']
-  ) {
-    console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
+    !req.variables['APPWRITE_FUNCTION_API_KEY'] 
+    ) {
+      console.log("Environment variables are not set. Function cannot use Appwrite SDK.");
   } else {
-    client
+      console.log(req.variables)
+      client
       .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
       .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
       .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
       .setSelfSigned(true);
   }
-
-  const databases = new Databases(client);
-
-  let documents = await databases.listDocuments("64645a31dbc67da320cf","64645a4ae7339e9f1cb5")
+  const databases = new Databases(client)
+  let data = await databases.listDocuments(req.variables.DatabaseID, req.variables.CollectionID)
 
   try {
     const Bot = new TwitterApi({
-      appKey: documents.appKey,
-      appSecret: documents.appSecret,
-      accessToken: documents.accessToken,
-      accessSecret: documents.accessSecret,
+      appKey: data.documents[0].appKey,
+      appSecret: data.documents[0].appSecret,
+      accessToken: data.documents[0].accessToken,
+      accessSecret: data.documents[0].accessSecret,
     });
+    const tweets = await Bot.v2.search("@QuotesBot687")
+    console.log(tweets)
   } catch(err) {
     console.log(documents)
     console.log(err)
   }
 
-  
   res.json({
     areDevelopersAwesome: true,
   });
